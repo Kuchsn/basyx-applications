2841e6b9fcd4b354eb60d9a57db390d4ffe55bd2
Maximilian Schlenz <maximilian.schlenz@protonmail.com>
2024-05-22 10:14:47 +0200

    removed unused components

--- dataintegrator/dataintegrator.aas/dataintegrator.aas.core/src/main/java/org/eclipse/digitaltwin/basyx/dataintegrator/aas/core/aggregator/ResultAggregator.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.digitaltwin.basyx.dataintegrator.aas.core.aggregator;
-
-public interface ResultAggregator<T> {
-	
-	public T getValue();
-
-}
--- dataintegrator/dataintegrator.common/dataintegrator.common.core/src/main/java/org/eclipse/digitaltwin/basyx/dataintegrator/core/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.digitaltwin.basyx.dataintegrator.core.utils;
-
-public class Constants {
-
-    public static final String FILE_NAME_CONTEXT_KEY = "fileName";
-}
